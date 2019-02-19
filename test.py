e860bf208646b04c3e26396ae5e3db35006f244d
ejcgarcia <47420547+ejcgarcia@users.noreply.github.com>
2019-02-19 17:03:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
+import main
+
+test_passed = True
+
 def test():
-    passed = True
-    result = xor(0,0)
-    if result == 1:
-        passed = False
-    result = xor(0,1)
-    if result == 0:
-        passed = False
-    result = xor(1,0)
-    if result == 0:
-        passed = False
-    result = xor(1,1)
-    if result == 1:
-        passed = False
-    return passed
+	passed = True
+	result = main.xor(0,0)
+	if result == 1:
+		passed = False
+	result = main.xor(0,1)
+	if result == 0:
+		passed = False
+	result = main.xor(1,0)
+	if result == 0:
+		passed = False
+	result = main.xor(1,1)
+	if result == 1:
+		passed = False
+	return passed
 
 test_passed = test()
 
 if test_passed == True:
-    print("Test passed! :)")
+	print("Test passed! :)")
 elif test_passed == False:
-    print("Test not passed :(")
+    	print("Test not passed :(")
